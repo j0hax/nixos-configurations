db480342c1aed7df579dd7f18b98d1cf59433fc4
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-05-03 18:39:12 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,11 @@
 {
-  description = "Johannes' NixOS Configurations";
+  description = "Johannes' NixOS Modules";
 
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
-    nixos-hardware.url = github:NixOS/nixos-hardware/master;
-    home-manager.url = github:nix-community/home-manager/master;
-  };
-
-  outputs = { self, nixpkgs, nixos-hardware, home-manager }: {
-    nixosConfigurations.eldridge = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nixos-hardware.nixosModules.common-cpu-intel-sandy-bridge
-        nixos-hardware.nixosModules.common-gpu-amd-sea-islands
-        nixos-hardware.nixosModules.common-pc-ssd
-        ./hardware-configuration.nix
-        ./system.nix
-        ./configuration.nix
-        ./packages.nix
-        ./environment.nix
-      ];
-    };
-
-    nixosConfigurations.kirby = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nixos-hardware.nixosModules.lenovo-thinkpad-x230
-        nixos-hardware.nixosModules.common-pc-ssd
-        ./hardware-configuration.nix
-        ./configuration.nix
-        ./system.nix
-        ./packages.nix
-        ./environment.nix
-      ];
-    };
-  };
+  outputs = { self }: {
+    nixosModules = {
+      environment = import ./environment.nix;
+      maintenance = import ./maintenance.nix;
+      packages = import ./packages.nix;
+      system = import ./system.nix;
+  }
 }
