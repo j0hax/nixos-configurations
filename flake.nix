eddf815a96e5a2ff233d1987e6ce88125e1970ce
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-05-11 23:45:50 +0200

    Include current configurations

--- flake.nix
+++ flake.nix
@@ -1,12 +1,40 @@
 {
-  description = "Johannes' NixOS Modules";
+  description = "Johannes' NixOS Configurations";
 
-  outputs = { self }: {
-    nixosModules = {
-      environment = import ./environment.nix;
-      maintenance = import ./maintenance.nix;
-      packages = import ./packages.nix;
-      system = import ./system.nix;
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    #home-manager.url = github:nix-community/home-manager/master;
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    nixosConfigurations.eldridge = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-hardware.nixosModules.common-cpu-intel-sandy-bridge
+        nixos-hardware.nixosModules.common-gpu-amd-sea-islands
+        nixos-hardware.nixosModules.common-pc-ssd
+        ./host-specific/eldridge/hardware-configuration.nix
+        ./host-specific/eldridge/configuration.nix
+        ./general/environment.nix
+        ./general/maintenance.nix
+        ./general/packages.nix
+        ./general/system.nix
+      ];
+    };
+
+    nixosConfigurations.kirby = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-hardware.nixosModules.lenovo-thinkpad-x230
+        nixos-hardware.nixosModules.common-pc-ssd
+        ./host-specific/eldridge/hardware-configuration.nix
+        ./host-specific/eldridge/configuration.nix
+        ./general/environment.nix
+        ./general/maintenance.nix
+        ./general/packages.nix
+        ./general/system.nix
+      ];
     };
   };
 }
---
+++ host-specific/kirby/configuration.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, ... }:
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.systemd-boot.consoleMode = "max";
+
+  # Use the Zen kernel
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
+  networking.hostName = "kirby"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  users.users.johannes = {
+    description = "Johannes Arnold";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "vboxusers" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # Enable the Plasma 5 Desktop Environment
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = "johannes";
+  };
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+
+  # Shell Preferences
+  programs.bash.promptInit = ''eval "$(starship init bash)"'';
+  programs.thefuck.enable = true;
+  environment.shellAliases = { "cat" = "bat"; };
+  environment.homeBinInPath = true;
+
+  # Maintenance
+  services.zfs.autoScrub.enable = true;
+  services.btrfs.autoScrub.enable = true;
+ 
+  services.usbmuxd.enable = true;
+  services.usbmuxd.user = "root";
+  services.usbmuxd.group = "root";
+
+  nixpkgs.overlays = [
+    (self: super: {
+      idevicerestore = super.idevicerestore.overrideAttrs (old: {
+        version = "newest";
+        src = super.fetchFromGitHub {
+          owner = "libimobiledevice";
+          repo = "idevicerestore";
+          rev = "4c154e82197039fd0991c69f563706bb17eb13ff";
+          sha256 = "sha256-RLt3lDP3jIDuLgG4eWNc8/bE1kJRXLNGy2+aNIyekuQ=";
+        };
+      });
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [ idevicerestore ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+}
+
