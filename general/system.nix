76abc0cf2d6fff9d07b2d64bc66f52bed69eaa70
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-05-27 00:18:43 +0200

    Configure automatic updates

--- general/system.nix
+++ general/system.nix
@@ -2,6 +2,12 @@
 {
   # Remote access is critical
   services.openssh.enable = true;
+  
+  # Auto-Upgrade the system
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:j0hax/nixos-configurations";
+  };
 
   # Add myself as a user
   users.users.johannes = {
