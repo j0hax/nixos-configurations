2757e61e99c012bca93e173125a0205df64849ea
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-06-18 00:17:50 +0200

    Set locale to Denglisch

--- general/environment.nix
+++ general/environment.nix
@@ -21,6 +21,15 @@
   programs.thefuck.enable = true;
   environment.shellAliases = { "cat" = "bat"; };
 
+  # Denglisch Locale ;)
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = lib.mkDefault {
+    LC_TIME = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+  };
+
   # Keyboard options
   services.xserver = lib.mkDefault {
     layout = "us";
