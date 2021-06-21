409ca21f294c48d6d270a77d778504d998e885ea
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-06-21 18:08:19 +0200

    Include prune options

--- general/backups.nix
+++ general/backups.nix
@@ -5,6 +5,12 @@
       repository = "sftp:johannes@eldridge.lan:/data/backups/";
       paths = [ "/home" "/var/lib" ];
       extraBackupArgs = [ "--exclude-caches" ];
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+        "--keep-yearly 75"
+      ];
       initialize = true;
       passwordFile = "/root/restic-password";
     };
