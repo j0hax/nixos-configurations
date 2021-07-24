cce6bed02ce9d438a52636caf03c7b06f6fb4368
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-07-24 14:04:10 +0200

    Simplify MOTD

--- general/environment.nix
+++ general/environment.nix
@@ -1,9 +1,6 @@
 { config, pkgs, lib, ... }: {
   users.motd = with config; ''
-    Welcome to ${networking.hostName}
-
-    - This server is managed by NixOS
-    - All changes are futile
+    Welcome to ${networking.hostName}!
 
     OS:      NixOS ${system.nixos.release} (${system.nixos.codeName})
     Version: ${system.nixos.version}
