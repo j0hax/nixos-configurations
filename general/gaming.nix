b54c19e566f78bb612cf6096d2cc2a8eb2a0102d
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-08-06 19:20:49 +0200

    Include Quake III Arena and SuperTuxKart

--- general/gaming.nix
+++ general/gaming.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ sauerbraten ];
+  environment.systemPackages = with pkgs; [ sauerbraten quake3e superTuxKart ];
 
   programs.steam.enable = true;
   programs.gamemode.enable = true;
