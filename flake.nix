d9e89874ab92e279834dc251f213b21beb355db4
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-08-23 01:12:39 +0200

    Switch to unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Johannes' NixOS Configurations";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
     #home-manager.url = github:nix-community/home-manager/master;
   };
