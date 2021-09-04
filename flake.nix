eff2404161e0d41ce708876912b13e63cbd67ec6
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-09-04 17:19:23 +0200

    Rewrite to handle configuration programmatically

--- flake.nix
+++ flake.nix
@@ -2,64 +2,43 @@
   description = "Johannes' NixOS Configurations";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
-    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     #home-manager.url = github:nix-community/home-manager/master;
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware }:
+    let
+      # Function to create (common) desktop system configuration
+      desktopConfig = hostname:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ({ ... }: {
+              imports = [
+                # Include host-specific configuration files and all modules
+                (import (./host-specific + "/${hostname}/configuration.nix"))
+                (import
+                  (./host-specific + "/${hostname}/hardware-configuration.nix"))
+              ] ++ (builtins.attrValues self.nixosModules);
+            })
+          ];
+        };
 
-    nixosModules = {
-      environment = import ./general/environment.nix;
-      maintenance = import ./general/maintenance.nix;
-      desktop-packages = import ./general/packages.nix;
-      system = import ./general/system.nix;
-      backups = import ./general/backups.nix;
-      fonts = import ./general/fonts.nix;
-      printing = import ./general/printing.nix;
-      tailscale = import ./general/tailscale.nix;
-      clamav = import ./general/clamav.nix;
-      virtualisation = import ./general/virtualisation.nix;
-      gaming = import ./general/gaming.nix;
-    };
+      # Read all modules from a specific folder
+      modulesFrom = dir:
+        builtins.listToAttrs (map (file: {
+          name = builtins.head (builtins.split "\\." file);
+          value = import (dir + ("/" + file));
+        }) (builtins.attrNames (builtins.readDir dir)));
+    in {
+      # Include everything from our general folder
+      nixosModules = (modulesFrom ./general);
 
-    nixosConfigurations.eldridge = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = with self.outputs.nixosModules; [
-        nixos-hardware.nixosModules.common-cpu-intel-sandy-bridge
-        nixos-hardware.nixosModules.common-gpu-amd-sea-islands
-        nixos-hardware.nixosModules.common-pc-ssd
-        ./host-specific/eldridge/hardware-configuration.nix
-        ./host-specific/eldridge/configuration.nix
-        system
-        environment
-        maintenance
-        desktop-packages
-        backups
-        fonts
-        tailscale
-      ];
+      # Configuration per host
+      nixosConfigurations = builtins.listToAttrs (map (host: {
+        name = host;
+        value = desktopConfig host;
+      }) (builtins.attrNames (builtins.readDir ./host-specific)));
     };
-
-    nixosConfigurations.kirby = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = with self.outputs.nixosModules; [
-        nixos-hardware.nixosModules.lenovo-thinkpad-x230
-        nixos-hardware.nixosModules.common-pc-ssd
-        ./host-specific/kirby/hardware-configuration.nix
-        ./host-specific/kirby/configuration.nix
-        system
-        environment
-        maintenance
-        desktop-packages
-        backups
-        fonts
-        printing
-        tailscale
-        gaming
-        virtualisation
-        ./general/distributed-builds.nix
-      ];
-    };
-  };
 }
