2ad455bf06a13e9f23ac32cc332afb3002820f7f
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-09-28 19:44:45 +0200

    Configure GameMode

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -8,7 +8,24 @@
   ];
 
   programs.steam.enable = true;
-  programs.gamemode.enable = true;
+  
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        renice = 20;
+      };
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 0;
+        amd_performance_level = "high";
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
 
   # Important for Wine
   hardware.opengl.driSupport32Bit = true;
