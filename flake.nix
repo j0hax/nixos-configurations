9ed305c39c3c87538ab9b60cb1fc275d2cf743be
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-10-15 21:14:00 +0200

    Revert "Create lib extension"
    This reverts commit ecfc6f0b10f0afad9e322deb8e2bb61917612ea0.

--- flake.nix
+++ flake.nix
@@ -7,13 +7,42 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager }: rec {
-    # Include everything from our general folder
-    nixosModules = lib.jx.modulesFrom ./modules;
+  outputs = { self, nixpkgs, nixos-hardware, home-manager }:
+    let
 
-    # Configuration per host
-    nixosConfigurations = lib.jx.configurationsFrom ./hosts;
+      lib = nixpkgs.lib;
 
-    lib = { jx = import ./lib { lib = nixpkgs.lib; }; };
-  };
+      hostsDir = ./hosts;
+
+      # Function to create (common) desktop system configuration
+      desktopConfig = { hostname, system ? "x86_64-linux" }:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs.nixos-hardware = self.inputs.nixos-hardware;
+          modules = [
+            ({ ... }: {
+              imports = [
+                # Include host-specific configuration files and all modules
+                (import (hostsDir + "/${hostname}/configuration.nix"))
+                home-manager.nixosModules.home-manager
+              ] ++ (lib.attrValues self.nixosModules);
+            })
+          ];
+        };
+
+      # Read all modules from a specific folder
+      modulesFrom = dir:
+        lib.listToAttrs (map (file: {
+          name = builtins.head (builtins.split "\\." file);
+          value = import (dir + "/${file}");
+        }) (lib.attrNames (builtins.readDir dir)));
+    in {
+      # Include everything from our general folder
+      nixosModules = (modulesFrom ./modules);
+
+      # Configuration per host
+      nixosConfigurations =
+        lib.genAttrs (lib.attrNames (builtins.readDir hostsDir))
+        (hostname: desktopConfig { inherit hostname; });
+    };
 }
