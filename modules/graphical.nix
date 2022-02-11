177d76ad716a194c3bab7952b824bf64d5041469
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-02-11 11:47:48 +0100

    Enable retroarch

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -2,7 +2,14 @@
   services.xserver = {
     enable = lib.mkDefault true;
     displayManager.sddm.enable = lib.mkDefault true;
-    desktopManager.plasma5.enable = lib.mkDefault true;
+    desktopManager = {
+      plasma5.enable = lib.mkDefault true;
+
+      retroarch = {
+        enable = lib.mkDefault true;
+        package = pkgs.retroarchFull;
+      };
+    };
   };
 
   programs.sway.enable = lib.mkDefault true;
