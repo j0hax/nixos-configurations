26304db218475ae751e9576017c59a8dc12bf5c5
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-02-16 21:57:01 +0100

    Init experimental dictionaries module

---
+++ modules/dictionaries.nix
@@ -0,0 +1,17 @@
+
+
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    hunspell
+    hunspellDicts.en_US-large
+    hunspellDicts.de_DE
+    hyphen
+  ];
+
+  environment.pathsToLink = [ "/share/hunspell" "/share/myspell" "/share/hyphen" ];
+
+  environment.variables.DICPATH = "/run/current-system/sw/share/hunspell:/run/current-system/sw/share/hyphen";
+}
+
