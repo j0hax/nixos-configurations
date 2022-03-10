6506e3ff094100477d6c6fd6ea3c8f55ace0c71f
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-03-10 22:57:37 +0100

    Include docker-compose

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }: {
   virtualisation.libvirtd.enable = lib.mkDefault true;
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-  environment.systemPackages = with pkgs; [ virt-manager lazydocker ];
+  environment.systemPackages = with pkgs; [ virt-manager lazydocker docker-compose ];
 
   virtualisation.docker = {
     enable = lib.mkDefault true;
