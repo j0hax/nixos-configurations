08b0e77e93a7188aaaf2573bdf407e421600e054
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-04-02 20:38:59 +0200

    Enable IOMMU

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,6 +1,11 @@
 { pkgs, lib, config, ... }: {
   virtualisation.libvirtd.enable = lib.mkDefault true;
-  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+
+  boot = {
+    kernelModules = [ "kvm-amd" "kvm-intel" ];
+    kernelParams = [ "intel_iommu=on" "intel_iommu=on" ];
+  };
+
   environment.systemPackages = with pkgs; [ virt-manager lazydocker docker-compose ];
 
   virtualisation.docker = {
