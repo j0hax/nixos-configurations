0f2c0cc2534108e1337bd4fe34b59d8a7bdb28ce
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-04-19 22:17:28 +0200

    Enable libvirt hostname resolution

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, config, ... }: {
   virtualisation.libvirtd.enable = lib.mkDefault true;
 
+  # Allow SSHing into virtual machines
+  system.nssDatabases.hosts = [ "libvirt" "libvirt_guest" ];
+
   boot = {
     kernelModules = [ "kvm-amd" "kvm-intel" ];
     kernelParams = [ "intel_iommu=on" "intel_iommu=on" ];
