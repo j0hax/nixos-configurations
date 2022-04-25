7650af3d6c65f93b4be6517e10b64604bcbef4bd
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-04-25 22:02:02 +0200

    Switch to restic

--- modules/backups.nix
+++ modules/backups.nix
@@ -1,19 +1,9 @@
 { config, ... }: {
-  services.borgbackup.jobs.documents = {
-    paths = "/home/johannes/Documents";
-    repo = "hgov5651@hgov5651.repo.borgbase.com:repo";
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat /root/backup-password";
+  services.restic = {
+    home = {
+      repository = "/run/media/johannes/Backups";
+      paths = [ "/home" "/root" ];
+      passwordFile = "/etc/nixos/restic-password";
     };
-    prune.keep = {
-      hourly = 24;
-      daily = 31;
-      weekly = 4;
-      monthly = -1;
-    };
-    extraPruneArgs = "--save-space";
-    startAt = "hourly";
-    compression = "auto,lzma";
   };
 }
