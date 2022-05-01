0cc786a0d4b21f86790851405a11ecde8629ebad
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-05-01 17:50:17 +0200

    Remove mounts

--- modules/mounts.nix
+++ modules/mounts.nix
@@ -1,10 +1,3 @@
 { config, ... }: {
   services.davfs2.enable = true;
-
-  fileSystems ={
-    "/mnt/seafile" = {
-      device = "https://seafile.cloud.uni-hannover.de/dav";
-      fsType = "davfs2";
-    };
-  };
 }
