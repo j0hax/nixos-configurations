663cb6a28dab1ab8d314341ab9d7c2db5a7aaff1
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-05-19 18:22:30 +0200

    Switch to esperanto

--- modules/environment.nix
+++ modules/environment.nix
@@ -21,13 +21,8 @@
   environment.localBinInPath = lib.mkDefault true;
 
   # Denglisch Locale ;)
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = lib.mkDefault {
-    LC_TIME = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-  };
+  i18n.defaultLocale = "eo.UTF-8";
+  i18n.extraLocaleSettings = lib.mkDefault { LC_MONETARY = "de_DE.UTF-8"; };
 
   # Keyboard options
   services.xserver = lib.mkDefault {
