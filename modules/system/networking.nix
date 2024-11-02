09b69500f402fdea04622c1484e3e838460439e2
Johannes Arnold <johannes@rnold.online>
2024-11-02 12:38:49 +0100

    Init systemd-resolved

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -4,9 +4,33 @@
   ...
 }:
 {
-  networking.networkmanager.wifi.backend = "iwd";
   services.mullvad-vpn = {
     enable = true;
     enableExcludeWrapper = true;
   };
+
+  # We want NetworkManager + encrypted DNS
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+    dns = "systemd-resolved";
+  };
+
+  services.resolved = {
+    enable = true;
+    dnssec = "allow-downgrade";
+    dnsovertls = "opportunistic";
+    fallbackDns = [
+      "1.1.1.1"
+      "8.8.8.8"
+      "9.9.9.9"
+    ];
+    llmnr = "true";
+    extraConfig = ''
+      Domains=~.
+      MulticastDNS=true
+    '';
+  };
+
+  environment.systemPackages = [ pkgs.dnsutils ];
 }
