aae817fc1d16787b6cdbb28274e8c24f89ec6bcd
Johannes Arnold <johannes@rnold.online>
2024-11-20 22:34:23 +0100

    Remove unecessary Gnome packages

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  config,
   ...
 }:
 {
@@ -10,6 +9,26 @@
     desktopManager.gnome.enable = true;
   };
 
+  # Remove the following Gnome apps
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gedit
+      cheese # webcam tool
+      gnome-music
+      gnome-terminal
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+    ]);
+
   programs.kdeconnect = {
     enable = true;
     package = pkgs.gnomeExtensions.gsconnect;
@@ -17,7 +36,16 @@
 
   environment.systemPackages = with pkgs; [
     transmission_4-gtk
+
+    # Extra gnome apps
     gnome-network-displays
-    gnomeExtensions.paperwm
+    impression # Bootable Drive Tool
+    gnome-decoder # QR Code Generator
+    metadata-cleaner
+    gnome-obfuscate
+    junction
+    eyedropper
+    shortwave
+    video-trimmer
   ];
 }
