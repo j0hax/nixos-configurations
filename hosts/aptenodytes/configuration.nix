fb14ce0337f338a19c7aa75c6a6fcbe90cfa99b3
Johannes Arnold <jarnold@b1-systems.de>
2025-04-13 17:51:13 +0200

    Remove default desktop environments

--- hosts/aptenodytes/configuration.nix
+++ hosts/aptenodytes/configuration.nix
@@ -35,12 +35,12 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
   
 
   # Configure keymap in X11
