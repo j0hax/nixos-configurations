2fffee3595db9765d8bfdb57ffc446881ed97af8
Johannes Arnold <jarnold@b1-systems.de>
2025-04-13 17:52:28 +0200

    Set preferred locales

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -15,6 +15,19 @@
   # Does NOT work with bcachefs yet...
   #boot.initrd.systemd.enable = true;
 
+  # Locale settings
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8"];
+  i18n.extraLocaleSettings = {
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  services.xserver.layout = "eu";
+
   nix.settings = {
     # When using a tmpfs, /tmp is often too small:
     # https://github.com/NixOS/nixpkgs/issues/293114#issuecomment-2663470083
