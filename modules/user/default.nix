df29e86c7741c2fed05b057e6c7112def836eb16
Johannes Arnold <jarnold@b1-systems.de>
2025-05-19 18:53:09 +0200

    Reorganize home-manager modules

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,4 +1,11 @@
-{ ... }:
+{ home-manager, ... }:
 {
-  services.homed.enable = true;
+  # Taken from https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nixos-module
+  imports = [
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+    }
+  ];
 }
--- modules/user/johannes/default.nix
+++ modules/user/johannes/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   pkgs,
-  home-manager,
   ...
 }:
 {
@@ -19,16 +18,9 @@
     ];
   };
 
+  home-manager.users.johannes = import ./home.nix;
+
   # Enable decryption of Secrets with my key
   age.identityPaths = [ "${config.users.users.johannes.home}/.ssh/id_ed25519" ];
 
-  # HM Stuff
-  imports = [
-    home-manager.nixosModules.home-manager
-    {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.users.johannes = import ./home.nix;
-    }
-  ];
 }
