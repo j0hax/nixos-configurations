41ec7c5fe51fc5d9628db70eac42c6ebddfbf766
Johannes Arnold <jarnold@b1-systems.de>
2025-06-30 23:09:55 +0200

    Remove Firefox configuration

--- modules/desktop/packages.nix
+++ modules/desktop/packages.nix
@@ -44,32 +44,6 @@
     ausweisapp.enable = true;
     thunderbird.enable = true;
     chromium.enable = true;
-    firefox = {
-      enable = true;
-      #package = pkgs.librewolf;
-      policies = {
-        DisableTelemetry = true;
-        DisableFirefoxStudies = true;
-        Preferences = {
-          "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
-          "cookiebanners.service.mode" = 2; # Block cookie banners
-          "privacy.donottrackheader.enabled" = true;
-          "privacy.fingerprintingProtection" = true;
-          "privacy.resistFingerprinting" = true;
-          "privacy.trackingprotection.emailtracking.enabled" = true;
-          "privacy.trackingprotection.enabled" = true;
-          "privacy.trackingprotection.fingerprinting.enabled" = true;
-          "privacy.trackingprotection.socialtracking.enabled" = true;
-        };
-        ExtensionSettings = {
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-          };
-        };
-      };
-    };
-
     steam.enable = true;
     adb.enable = true;
     appimage = {
