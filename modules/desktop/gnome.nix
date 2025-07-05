fabe6e43efd9697c5b47a0560c64ec15bf673971
Johannes Arnold <jarnold@b1-systems.de>
2025-07-05 19:17:34 +0200

    Comment extra GNOME packages

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -7,31 +7,30 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
-   
+
   programs.kdeconnect = {
     enable = true;
     package = pkgs.gnomeExtensions.gsconnect;
   };
 
+  # Additional GTK Packages
   environment.systemPackages = with pkgs; [
     transmission_4-gtk
     gnome-network-displays
     impression # Bootable Drive Tool
     gnome-decoder # QR Code Generator
     metadata-cleaner
-    gnome-obfuscate
+    gnome-obfuscate # Redaction tool
     eyedropper
-    shortwave
+    shortwave # Internet radio
     video-trimmer
-    #flare-signal # buggy for now
-    #varia
+    varia
     bottles
     gnome-2048
     gnome-firmware
-    gnome-solanum
-    helvum
-    foliate
-    dosage-tracker
-    deja-dup
+    gnome-solanum # Pomodoro timer
+    helvum # Patchbay
+    foliate # E-Reader
+    deja-dup # Backup tool
   ];
 }
