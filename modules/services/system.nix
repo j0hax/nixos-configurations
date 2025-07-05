f5c9ec03d654ff031fc585f8b4b5fabd1d0a6961
Johannes Arnold <jarnold@b1-systems.de>
2025-07-05 19:12:19 +0200

    Remove TOR

--- modules/services/system.nix
+++ modules/services/system.nix
@@ -9,10 +9,5 @@
     openssh.enable = true;
     tuptime.enable = true;
     smartd.enable = true;
-    tor = {
-      enable = true;
-      client.enable = true;
-      torsocks.enable = true;
-    };
   };
 }
