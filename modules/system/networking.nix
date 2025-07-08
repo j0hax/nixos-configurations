f906f0e5a3a260a3ead8893142fc4d9bbd57ab99
Johannes Arnold <jarnold@b1-systems.de>
2025-07-08 23:50:52 +0200

    Set up Quad9 DNS over TLS

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -16,20 +16,28 @@
     dns = "systemd-resolved";
   };
 
+  /*
+    networking.nameservers = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
+  */
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+  ];
+
   services.resolved = {
     enable = true;
-    dnssec = "allow-downgrade";
-    dnsovertls = "opportunistic";
+    dnssec = "true";
+    dnsovertls = "true";
     fallbackDns = [
-      "1.1.1.1"
-      "8.8.8.8"
-      "9.9.9.9"
+      "9.9.9.9#dns.quad9.net"
+      "149.112.112.112#dns.quad9.net"
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
     ];
-    llmnr = "true";
-    extraConfig = ''
-      Domains=~.
-      MulticastDNS=true
-    '';
+    domains = [ "~." ];
   };
 
   environment.systemPackages = [ pkgs.dnsutils ];
