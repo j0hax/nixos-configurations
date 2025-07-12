09aae1728d1264c544022abf9d754ed85df88016
Johannes Arnold <jarnold@b1-systems.de>
2025-07-12 19:27:30 +0200

    Add large number of supplementary fonts

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -8,11 +8,35 @@
     packages =
       with pkgs;
       [
+        # Generally nice
         inter
         libertinus
         iosevka
+        crimson-pro
+        eb-garamond
+        libre-caslon
+        cardo
+        gentium
+        junicode
+        font-awesome
+
+        # Frisia
         montserrat
+        gyre-fonts
+
+        # Company
         paratype-pt-sans
+
+        # Source
+        source-sans
+        source-serif
+        source-code-pro
+
+        # Good collections
+        league-of-moveable-type
+        open-fonts
+        dotcolon-fonts
+        inriafonts
       ]
       ++ builtins.map (v: iosevka-bin.override { variant = v; }) [
         ""
