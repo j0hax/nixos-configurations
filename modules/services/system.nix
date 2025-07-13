c43e827a92629a21a2c5411aa40c4d28e3c7e59d
Johannes Arnold <jarnold@b1-systems.de>
2025-07-13 14:11:23 +0000

    Disable password authentication

--- modules/services/system.nix
+++ modules/services/system.nix
@@ -7,7 +7,14 @@
   services = lib.mkDefault {
     locate.enable = true;
     fwupd.enable = true;
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      settings = {
+        # Disable password authentication
+        PasswordAuthentication = false;
+        ChallengeResponseAuthentication = false;
+      };
+    };
     tuptime.enable = true;
     smartd.enable = true;
   };
