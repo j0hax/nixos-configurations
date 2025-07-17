96ce6b0953100621dc24889a1c7abf68776bf499
Johannes Arnold <jarnold@b1-systems.de>
2025-07-17 18:02:43 +0200

    Disable root user and password-based authentication

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -5,4 +5,13 @@
     sudo.enable = false;
     sudo-rs.enable = true;
   };
+
+  # Disable root user
+  users.users.root.hashedPassword = "!";
+
+  # Disable password login for SSH
+  settings = {
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+  };
 }
