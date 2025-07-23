a705456520537daeb76a8eaa578b79c75832f9a1
Johannes Arnold <jarnold@b1-systems.de>
2025-07-23 20:04:47 +0000

    RIP Ozzy

--- modules/server/navidrome.nix
+++ modules/server/navidrome.nix
@@ -4,6 +4,7 @@
 
   services.caddy.virtualHosts."music.jka.one".extraConfig = ''
     reverse_proxy 127.0.0.1:${toString config.services.navidrome.settings.Port}
+    header X-Clacks-Overhead "GNU Ozzy Osbourne"
   '';
 
   services.navidrome = {
