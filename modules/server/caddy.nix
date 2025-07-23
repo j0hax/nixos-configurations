4a8b61f0cb6a30217094297c1f894a678118fb43
Johannes Arnold <jarnold@b1-systems.de>
2025-07-23 07:13:40 +0000

    Restructure server modules, add glance

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,9 +1,18 @@
-{ ... }:
+{ lib, config, ... }:
 {
-  services.caddy = {
+  services.caddy = lib.mkDefault {
     enable = true;
+    email = "johannes@rnold.online";
     globalConfig = ''
       encode zstd gzip
+      header {
+        X-Clacks-Overhead GNU Ozzy Osbourne
+      }
+    '';
+
+    # Remove www subdomain
+    virtualHosts."www.${config.networking.fqdn}".extraConfig = ''
+      redir https://example.com{uri}
     '';
   };
 
--- modules/server/jellyfin.nix
+++ modules/server/jellyfin.nix
@@ -1,8 +1,10 @@
 { ... }:
 {
-  services.jellyfin.enable = true;
-
+  imports = [ ./caddy.nix ];
+  
   services.caddy.virtualHosts."jellyfin.jka.one".extraConfig = ''
     reverse_proxy 127.0.0.1:8096
   '';
+
+  services.jellyfin.enable = true;
 }
