507ddd87240b71f41da10f82d531f62c9bcad69b
Johannes Arnold <jarnold@b1-systems.de>
2025-07-23 07:14:13 +0000

    Reformat

--- modules/user/johannes/hyprland.nix
+++ modules/user/johannes/hyprland.nix
@@ -52,34 +52,33 @@
         "$mod, mouse:273, resizewindow"
       ];
 
-      bind =
-        [
-          ", Print, exec, grimblast copy area"
-          "$mod, RETURN, exec, $terminal"
-          "$mod SHIFT, Q, exit"
-          "$mod, D, exec, $menu"
-          "$mod, F, fullscreen"
-          "$mod, J, togglesplit"
-          "$mod, P, pseudo"
-          "$mod, SPACE, togglefloating"
-          "$mod SHIFT, P, exec, ${lib.getExe pkgs.grim} -g $(${lib.getExe pkgs.slurp})"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (
-            builtins.genList (
-              i:
-              let
-                ws = i + 1;
-              in
-              [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            ) 9
-          )
-        );
+      bind = [
+        ", Print, exec, grimblast copy area"
+        "$mod, RETURN, exec, $terminal"
+        "$mod SHIFT, Q, exit"
+        "$mod, D, exec, $menu"
+        "$mod, F, fullscreen"
+        "$mod, J, togglesplit"
+        "$mod, P, pseudo"
+        "$mod, SPACE, togglefloating"
+        "$mod SHIFT, P, exec, ${lib.getExe pkgs.grim} -g $(${lib.getExe pkgs.slurp})"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (
+          builtins.genList (
+            i:
+            let
+              ws = i + 1;
+            in
+            [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          ) 9
+        )
+      );
 
       bindl = [
         ", switch:on:Lid Switch, exec, hyprctl keyword monitor eDP-1, disable"
