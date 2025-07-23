310d23a68641dc2701ee8adcfbca1bcfaa8c2ad0
Johannes Arnold <jarnold@b1-systems.de>
2025-07-23 07:14:30 +0000

    Add domain

--- hosts/skylab/configuration.nix
+++ hosts/skylab/configuration.nix
@@ -23,6 +23,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # networking.hostName = "nixos"; # Define your hostname.
+  networking.domain = "jka.one";
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
