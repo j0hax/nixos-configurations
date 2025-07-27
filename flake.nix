0052ba412c7d4111a1a631f905a5e83767c80859
Johannes Arnold <jarnold@b1-systems.de>
2025-07-27 17:32:41 +0000

    Remove audiobookshelf for now

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
             ./modules/user/johannes
             ./modules/server/jellyfin.nix
             ./modules/server/glance.nix
-            ./modules/server/audiobookshelf.nix
+            ./modules/server/bin.nix
             ./modules/server/navidrome.nix
           ];
         };
