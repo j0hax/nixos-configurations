a259e243c6e000a39543e8b45d00bba0d1119732
Johannes Arnold <jarnold@b1-systems.de>
2025-08-28 09:28:36 +0000

    Add SSH banner

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -10,9 +10,18 @@
   users.users.root.hashedPassword = "!";
 
   # Disable password login for SSH
-  services.openssh.settings = {
-    PasswordAuthentication = false;
-    KbdInteractiveAuthentication = false;
+  services.openssh = {
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+    banner = ''
+    ┌──────────────────────────────────────────────────────────────────────────┐
+    │ This system is for authorized users only. All activities on this system  │
+    │ are monitored and logged. Unauthorized access is strictly prohibited and │
+    │ will be prosecuted to the full extent of the law.                        │
+    └──────────────────────────────────────────────────────────────────────────┘
+    '';
   };
 
   services.fail2ban = {
