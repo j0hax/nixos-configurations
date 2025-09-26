aaaaeaab751abede8dd40cb4fdc86d94ffeb2e7a
Johannes Arnold <jarnold@b1-systems.de>
2025-09-26 17:23:40 +0000

    Switch to systemd-networkd

--- modules/server/wireguard.nix
+++ modules/server/wireguard.nix
@@ -1,88 +1,64 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
   port = 5267;
   addr = "wg.jka.one";
 in
 {
-  # networking.nat.enable = true;
-  # networking.nat.externalInterface = "enp1s0";
-  # networking.nat.internalInterfaces = [ "wg0" ];
   networking.firewall.allowedUDPPorts = [ 51820 ];
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv4.tcp_congestion_control" = "bbr";
-  };
-  /*
-    networking.wg-quick.interfaces = {
-      wg0 = {
-        address = [ "10.0.0.1/24" ];
-        listenPort = 51820;
-        privateKeyFile = "/etc/wireguard/privatekey";
 
-        peers = [
+  # Needed to prevent conflichts with systemd-network
+  networking.useNetworkd = true;
+
+  # Wireguard Server with systemd-networkd
+  systemd.network = {
+    enable = true;
+    netdevs = {
+      "50-wg0" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "wg0";
+        };
+
+        wireguardConfig = {
+          PrivateKeyFile = "/etc/wireguard/privatekey";
+          ListenPort = 51820;
+          RouteTable = "main"; # wg-quick creates routing entries automatically but we must use use this option in systemd.
+        };
+
+        wireguardPeers = [
           {
-            publicKey = "hyLhDEyI9Dxialfyc13Fs4j/ef1K+aTQ5tnu/wnXCEA=";
-            allowedIPs = [ "10.0.0.2/32" ];
+            PublicKey = "hyLhDEyI9Dxialfyc13Fs4j/ef1K+aTQ5tnu/wnXCEA=";
+            AllowedIPs = [ "10.0.0.2/32" ];
           }
           {
-            publicKey = "B0R6dJt7NGpM6IsUJy77MXtikU/F8cJwAtt4zkzyVXc=";
-            allowedIPs = [ "10.0.0.3/32" ];
+            PublicKey = "zZWEMegtMvP9taLRtHXyRwQtfOUv9oooRG/06f1Be2U=";
+            AllowedIPs = [ "10.0.0.3/32" ];
+          }
+          {
+            PublicKey = "X0kSkNPm071xtB2Ez9wqNf8bvWSifu0fUHtolIrrIhU=";
+            AllowedIPs = [ "10.0.0.4/32" ];
+          }
+          {
+            PublicKey = "x9NBa/ywMCPa0fRIb7o86msAHOiGFzri/dLADG3Qnnw=";
+            AllowedIPs = [ "10.0.0.5/32" "192.168.1.0/24" ];
           }
         ];
       };
     };
-  */
 
-  networking.firewall.trustedInterfaces = [ "wg0" ];
-
-  networking.wireguard.interfaces = {
-    wg0 = {
-      ips = [ "10.0.0.1/24" ];
-      listenPort = 51820;
-      privateKeyFile = "/etc/wireguard/privatekey";
-      peers = [
-        {
-          name = "aptenodytes";
-          publicKey = "hyLhDEyI9Dxialfyc13Fs4j/ef1K+aTQ5tnu/wnXCEA=";
-          allowedIPs = [ "10.0.0.2/32" ];
-        }
-        {
-          name = "webcam";
-          publicKey = "zZWEMegtMvP9taLRtHXyRwQtfOUv9oooRG/06f1Be2U=";
-          allowedIPs = [ "10.0.0.3/32" ];
-        }
-        {
-          name = "pixel";
-          publicKey = "X0kSkNPm071xtB2Ez9wqNf8bvWSifu0fUHtolIrrIhU=";
-          allowedIPs = [ "10.0.0.4/32" ];
-        }
-      ];
+    networks."wg0" = {
+      matchConfig.Name = "wg0";
+      address = [ "10.0.0.1/24" ];
+      networkConfig = {
+        IPMasquerade = "both";
+        IPv4Forwarding = true;
+        IPv6Forwarding = true;
+      };
     };
   };
-
-  # networking.nftables.tables.wireguard-hub = {
-  #   family = "inet";
-  #   content = ''
-  #     iifname $wg_iface oifname $wg_iface accept
-  #   '';
-  # };
-
-  /*
-    imports = [ ./caddy.nix ];
-
-    services.caddy.virtualHosts.${addr}.extraConfig = ''
-      reverse_proxy 127.0.01:${toString port}
-      encode zstd gzip
-      header X-Robots-Tag "noindex, nofollow"
-      cache
-    '';
-
-    services.wg-access-server = {
-      enable = true;
-      settings = {
-        port = port;
-      };
-      secretsFile = /etc/wireguard/wg-access-server-secrets.yaml;
-    };
-  */
 }
