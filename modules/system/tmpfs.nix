ce0e8fcad25fd37757449cd9834f8f3661a84b37
Johannes Arnold <jarnold@b1-systems.de>
2025-11-17 15:52:44 +0100

    Remove tmpfs settings for nix-daemon

--- modules/system/tmpfs.nix
+++ modules/system/tmpfs.nix
@@ -7,8 +7,4 @@
 {
   boot.tmp.cleanOnBoot = true;
   boot.tmp.useTmpfs = true;
-  systemd.services.nix-daemon = {
-    environment.TMPDIR = "/var/tmp";
-    serviceConfig.LimitNOFILE = lib.mkForce "infinity";
-  };
 }
