171a507be220025c735c924ef780fb03b2d41b5a
Johannes Arnold <jarnold@b1-systems.de>
2026-01-01 21:02:05 +0100

    Test

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
         {
           sops.defaultSopsFile = ./secrets.yaml;
           sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+          sops.secrets."turn/auth-secret" = {};
         }
       ];
 
