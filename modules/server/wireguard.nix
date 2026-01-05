cb0c937ada7f432c242d6d077d559071b6f3352a
Johannes Arnold <jarnold@b1-systems.de>
2026-01-05 16:09:55 +0000

    Enable IP Forwarding

--- modules/server/wireguard.nix
+++ modules/server/wireguard.nix
@@ -16,6 +16,11 @@ let
   };
 in
 {
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+  
   networking.firewall.allowedUDPPorts = [
     51820
   ];
