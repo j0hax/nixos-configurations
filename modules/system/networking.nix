0178e74f5b1dbcaf39dcf469618f5eec6cce8dc0
Johannes Arnold <jarnold@b1-systems.de>
2026-01-31 21:19:26 +0000

    Update DNS settings

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -22,7 +22,9 @@
   services.resolved = {
     enable = true;
     settings.Resolve = {
-      DNSOverTLS = "opportunistic";
+      DNS = [ "9.9.9.9#dns.quad9.net" "149.112.112.112#dns.quad9.net" "2620:fe::fe#dns.quad9.net" "2620:fe::9#dns.quad9.net" ];
+      DNSOverTLS = true;
+      DNSSEC = true;
       Domains = [ "~." ];
     };
   };
