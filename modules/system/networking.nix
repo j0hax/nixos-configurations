4ae25924031df38fc18b7e32336e67e608fcdc4c
Johannes Arnold <jarnold@b1-systems.de>
2026-02-02 17:15:07 +0100

    Prefer secure DNS

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -22,10 +22,8 @@
   services.resolved = {
     enable = true;
     settings.Resolve = {
-      DNS = [ "9.9.9.9#dns.quad9.net" "149.112.112.112#dns.quad9.net" "2620:fe::fe#dns.quad9.net" "2620:fe::9#dns.quad9.net" ];
-      DNSOverTLS = true;
-      DNSSEC = true;
-      Domains = [ "~." ];
+      DNSOverTLS = "opportunistic";
+      DNSSEC = "allow-downgrade";
     };
   };
 
