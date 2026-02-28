f32360c2fbe9e8dff76eb374b69504c0d90051ce
Johannes Arnold <jarnold@b1-systems.de>
2026-02-28 22:53:32 +0000

    Add flix subdomain

--- modules/server/jellyfin.nix
+++ modules/server/jellyfin.nix
@@ -1,8 +1,11 @@
 { ... }:
-{
+let
+  domainBase = "jka.one";
+in {
   imports = [ ./caddy.nix ];
 
-  services.caddy.virtualHosts."jellyfin.jka.one".extraConfig = ''
+  services.caddy.virtualHosts."jellyfin.${domainBase}".extraConfig = ''
+    serverAliases = [ "flix.${domainBase}" ];
     reverse_proxy 127.0.0.1:8096
   '';
 
