8b31f79ef95b0d6fab081bd6127e2b663e88f7fd
Johannes Arnold <jarnold@b1-systems.de>
2026-02-28 22:56:06 +0000

    Fix serverAlias

--- modules/server/jellyfin.nix
+++ modules/server/jellyfin.nix
@@ -4,10 +4,12 @@ let
 in {
   imports = [ ./caddy.nix ];
 
-  services.caddy.virtualHosts."jellyfin.${domainBase}".extraConfig = ''
+  services.caddy.virtualHosts."jellyfin.${domainBase}" = {
     serverAliases = [ "flix.${domainBase}" ];
-    reverse_proxy 127.0.0.1:8096
-  '';
+    extraConfig = ''
+      reverse_proxy 127.0.0.1:8096
+    '';
+  };
 
   services.jellyfin.enable = true;
 }
