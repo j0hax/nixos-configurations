88ebada40e254876aa423e2d81b12d6a3f75ff1b
Johannes Arnold <jarnold@b1-systems.de>
2026-03-09 19:55:48 +0000

    Disable root login

--- hosts/kneippweg/configuration.nix
+++ hosts/kneippweg/configuration.nix
@@ -91,7 +91,6 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-services.openssh.settings.PermitRootLogin = "yes";
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
