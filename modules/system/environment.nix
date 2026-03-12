6747ce4f2135dd59061215c26fdbf76ac22f9ac6
Johannes Arnold <jarnold@b1-systems.de>
2026-03-13 00:45:18 +0100

    Tweak locales

--- modules/system/environment.nix
+++ modules/system/environment.nix
@@ -6,15 +6,14 @@
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
     "de_DE.UTF-8/UTF-8"
+    "en_DK.UTF-8/UTF-8"
   ];
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_DK.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_ADDRESS = "de_DE.UTF-8";
     LC_MONETARY = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
   };
 
   # The best keyboard layout you have never heard of
