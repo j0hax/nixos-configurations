1a92203501f64082f164db79ee20da151ff8688f
Johannes Arnold <jarnold@b1-systems.de>
2026-03-15 09:21:14 +0000

    Reformat

--- hosts/kneippweg/configuration.nix
+++ hosts/kneippweg/configuration.nix
@@ -2,13 +2,18 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -37,11 +42,9 @@
   # Enable the X11 windowing system.
   #services.xserver.enable = true;
 
-
   # Enable the GNOME Desktop Environment.
   #services.displayManager.gdm.enable = true;
   #services.desktopManager.gnome.enable = true;
-  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -131,4 +134,3 @@
   };
 
 }
-
