6c3851af07df5b7ce85ed1c8bc5da17fc0ee9870
Johannes Arnold <jarnold@b1-systems.de>
2026-06-10 17:42:19 +0200

    Write shell script to swap files

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -84,5 +84,53 @@
 
       echo "Created \"$dest\""
     '')
+
+    (writeShellScriptBin "swap" ''
+      usage() {
+        printf 'Usage: %s <file1> <file2>\n' "$0" >&2
+      }
+
+      ensure_exists() {
+        [ -e "$1" ] || {
+          printf 'Error: %s does not exist\n' "$1" >&2
+          usage
+          exit 1
+        }
+      }
+      
+      if [ "$#" -ne 2 ]; then
+        usage
+        exit 1
+      fi
+
+      a="$1"
+      b="$2"
+
+      [ "$(realpath -- "$a")" != "$(realpath -- "$b")" ] || {
+        printf 'Error: files must be different\n' >&2
+        exit 1
+      }
+
+      ensure_exists "$a"
+      ensure_exists "$b"
+
+      # Generate a temporary file in the cwd
+      tmp=$(mktemp --tmpdir="$(dirname -- "$a")")
+
+      mv -v -- "$a" "$tmp" &&
+      {
+          # Move b to a, roll back if there is a failure!
+          mv -v -- "$b" "$a" ||
+          {
+              printf 'Error: could not move %s to %s, rolling back!\n' "$b" "$a" >&2
+              mv -v -- "$tmp" "$a"
+              exit 1
+          }
+      } &&
+      if ! mv -v -- "$tmp" "$a"; then
+        printf 'Rollback failed! Original file remains at %s\n' "$tmp" >&2
+        exit 2
+      fi
+    '')
   ];
 }
