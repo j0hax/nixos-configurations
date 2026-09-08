98c1d759a74ffaa2258f18e300fedf25acb515f0
Johannes Arnold <jarnold@b1-systems.de>
2026-09-08 20:52:59 +0000

    Add missing toString

--- hosts/skylab/configuration.nix
+++ hosts/skylab/configuration.nix
@@ -201,7 +201,7 @@ in
         # Minecraft server proxy
         tcp/:1902 {
           route {
-            proxy kneippweg.ygg.jka.one:${config.jka.services.minecraft.port}
+            proxy kneippweg.ygg.jka.one:${toString config.jka.services.minecraft.port}
           }
         }
       }
